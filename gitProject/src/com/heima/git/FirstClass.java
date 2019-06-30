1f2bc6eba15771ba2dc8c1adb8233deef4a20856
fanlq_git <fanlq_git@qq.com>
2019-07-01 00:15:08 +0800

    git first

---
+++ gitProject/src/com/heima/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.heima.git;
+
+/**
+ * @author flq
+ * @date 2019/6/30 0030 - 下午 11:58
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+}
